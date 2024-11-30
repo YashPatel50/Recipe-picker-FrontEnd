2beecfed984da97782d09cea3d2debf74ece1e2c
YashPatel50 <patelshay8@gmail.com>
2024-11-30 17:23:06 -0500

    Attempt to make a get request from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
+import backendClient from "./services/backend-api";
+
 function App() {
+  backendClient.get("/recipes/").then((res) => {
+    console.log(res);
+  });
   return (
     <>
       <div>Hello World</div>
