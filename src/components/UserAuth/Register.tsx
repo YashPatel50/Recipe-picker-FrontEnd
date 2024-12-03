fa74aab5a272be9b65fc3eff522e486d311b401f
YashPatel50 <patelshay8@gmail.com>
2024-12-02 20:23:57 -0500

    Started making form, centered a box. Need to create a react hook form

--- src/components/UserAuth/Register.tsx
+++ src/components/UserAuth/Register.tsx
@@ -1,10 +1,34 @@
-import { Button } from "@/components/ui/button";
+import { Box, Flex, Input, Stack } from "@chakra-ui/react";
+import { Field } from "@/components/ui/field";
+import { Center } from "@chakra-ui/react";
 
 const Register = () => {
   return (
-    <>
-      <Button>New Button</Button>
-    </>
+    <Flex>
+      <Center
+        width={"100vw"}
+        height={"100vh"}
+        alignContent={"center"}
+        justifyContent={"center"}
+      >
+        <Box
+          bg="gray.800"
+          width="50%"
+          height="70%"
+          padding={10}
+          rounded={30}
+          shadow={"sm"}
+        >
+          <form>
+            <Stack>
+              <Field label="Username">
+                <Input placeholder="Enter Username"></Input>
+              </Field>
+            </Stack>
+          </form>
+        </Box>
+      </Center>
+    </Flex>
   );
 };
 
