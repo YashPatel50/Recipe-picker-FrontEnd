6f45a0fb8ba94146c389ee131619a49a49092a7a
YashPatel50 <patelshay8@gmail.com>
2024-12-03 20:36:43 -0500

    Created a form with validation

--- src/components/UserAuth/Register.tsx
+++ src/components/UserAuth/Register.tsx
@@ -1,8 +1,34 @@
-import { Box, Flex, Input, Stack } from "@chakra-ui/react";
+import { Box, Button, Flex, Input, Stack } from "@chakra-ui/react";
 import { Field } from "@/components/ui/field";
 import { Center } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  username: z.string().regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$/, {
+    message:
+      "Username must be 8-20 characters. At least 1 Letter. At least 1 Number.  No Special characters",
+  }),
+  password: z
+    .string()
+    .regex(/^(?=.*[A-Z])(?=.*[\W_])(?=.*\d)[A-Za-z\d\W_]{8,20}$/, {
+      message:
+        "Password must be 8-20 characters long, include at least one uppercase letter, one special character, and one number.",
+    }),
+});
+
+type LoginFormData = z.infer<typeof schema>;
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<LoginFormData>({
+    resolver: zodResolver(schema),
+  });
+
   return (
     <Flex>
       <Center
@@ -19,11 +45,35 @@ const Register = () => {
           rounded={30}
           shadow={"sm"}
         >
-          <form>
+          <form onSubmit={handleSubmit((data) => console.log(data))}>
             <Stack>
-              <Field label="Username">
-                <Input placeholder="Enter Username"></Input>
+              <h1>Register</h1>
+              <Field label="Username" required>
+                <Input
+                  {...register("username")}
+                  placeholder="Enter Username"
+                ></Input>
+                {errors.username && (
+                  <p className="text-danger">{errors.username.message}</p>
+                )}
               </Field>
+              <Field label="Password" required>
+                <Input
+                  {...register("password")}
+                  placeholder="Enter Password"
+                ></Input>
+                {errors.password && (
+                  <p className="text-danger">{errors.password.message}</p>
+                )}
+              </Field>
+              <Button
+                disabled={!isValid}
+                mt={4}
+                colorScheme="teal"
+                type="submit"
+              >
+                Submit
+              </Button>
             </Stack>
           </form>
         </Box>
